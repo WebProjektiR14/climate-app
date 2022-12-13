ae90ca0efe5ee047ba66ff90a731786c9e932099
Janne <98741721+Jlallii@users.noreply.github.com>
2022-12-13 14:20:55 +0200

    Delete Backend/auth_template directory

--- Backend/auth_template/src/main/java/com/security/auth/MyPasswordEncoder.java
+++
@@ -1,9 +0,0 @@
-package com.security.auth;
-
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
-
-@Service
-public class MyPasswordEncoder extends BCryptPasswordEncoder{
-
-}
